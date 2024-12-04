aa8fe730d73c6eccef6d5f472a3f308190927cf7
Lucas chen <lucas.chen@gbo-asia.com>
2024-12-04 14:36:29 +0800

    initialize state in app

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use bevy::prelude::*;
-use flappy_bird::camera;
+use flappy_bird::{camera, states::GameState};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(camera::plugin)
+        .init_state::<GameState>()
         .run();
 }
