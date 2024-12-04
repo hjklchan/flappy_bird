d36ee2afdac39f90ef80884324f15640b6846e5f
Illusion <1161524405@qq.com>
2024-12-04 19:03:47 +0800

    add common system func

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,25 @@
+pub mod common;
 pub mod components;
 pub mod states;
 pub mod menu;
 pub mod camera;
 pub mod game;
+
+pub struct Hero {
+    pub key: &'static str,
+    pub name: &'static str,
+    pub image: &'static str,
+}
+
+pub const HERO_LIST: [Hero; 2] = [
+    Hero {
+        key: "hz",
+        name: "Huang Zhao",
+        image: "hz.png",
+    },
+    Hero {
+        key: "xmy",
+        name: "Xiao Ming Yan",
+        image: "xmy.png",
+    }
+];
