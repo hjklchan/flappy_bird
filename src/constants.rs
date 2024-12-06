8b57d5b71f8acd02f69682b4f14b06d090c66701
Lucas chen <lucas.chen@gbo-asia.com>
2024-12-06 14:12:47 +0800

    add commonly used constants

--- src/constants.rs
+++ src/constants.rs
@@ -2,6 +2,8 @@
 pub const WINDOW_WIDTH: f32 = 800.0;
 pub const WINDOW_HEIGHT: f32 = 512.0;
 
+pub const PIPE_WIDTH: f32 = 52.0;
+pub const PIPE_HALF_WIDTH: f32 = PIPE_WIDTH / 2.0;
 pub const PIPE_HEIGHT: f32 = 320.0;
 pub const PIPE_HALF_HEIGHT: f32 = PIPE_HEIGHT / 2.0;
 
