1462f6e55e07105c9f6f2b7a6eac9ef399ecb313
Lucas chen <lucas.chen@gbo-asia.com>
2024-12-06 14:13:21 +0800

    add restart if state in game over

--- src/game.rs
+++ src/game.rs
@@ -1,16 +1,31 @@
 use bevy::prelude::*;
 
-use crate::{states::GameState, Game};
+use crate::{
+    states::{GameState, PlayingState},
+    Game,
+};
 
 pub fn plugin(app: &mut App) {
-    app.add_plugins(GamePlugin);
+    app.add_plugins(GamePlugin {
+        enable_restart_game: false,
+    });
 }
 
-struct GamePlugin;
+struct GamePlugin {
+    // Debug options...
+    enable_restart_game: bool,
+}
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Update, press_key_to_start_game.run_if(is_select_hero));
+
+        if self.enable_restart_game {
+            app.add_systems(
+                Update,
+                press_space_restart_game.run_if(in_state(PlayingState::GameOver)),
+            );
+        }
     }
 }
 
@@ -28,4 +43,14 @@ fn press_key_to_start_game(
 // Check to see if the hero is selected
 fn is_select_hero(game: Res<Game>) -> bool {
     game.selected_hero.is_some()
+}
+
+fn press_space_restart_game(
+    button_input: Res<ButtonInput<KeyCode>>,
+    mut next_playing_state: ResMut<NextState<PlayingState>>,
+) {
+    if button_input.just_pressed(KeyCode::Space) {
+        // Just change the state to [`GameState::Ready`]
+        next_playing_state.set(PlayingState::Ready);
+    }
 }
