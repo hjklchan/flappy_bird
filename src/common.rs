08bd681d9e8313151e403b1d5ea6baaed390c884
Lucas chen <lucas.chen@gbo-asia.com>
2024-12-06 14:50:30 +0800

    define a new function that similar to in_state

--- src/common.rs
+++ src/common.rs
@@ -5,3 +5,24 @@ pub fn despawn_screen<T: Component>(to_despawn: Query<Entity, With<T>>, mut comm
         commands.entity(entity).despawn_recursive();
     }
 }
+
+/// ## Custom extension functions created for Bevy
+pub mod condition_pro {
+    use super::*;
+
+    /// Similar to in_state,
+    /// it's just an inverse operation
+    /// 
+    /// ### For example:
+    /// 
+    /// ```rust
+    /// // !in_state(PlayingState::GameOver)
+    /// some_system.run_if(not_in_state(PlayingState::GameOver));
+    /// ```
+    pub fn not_in_state<S: States>(state: S) -> impl FnMut(Option<Res<State<S>>>) -> bool + Clone {
+        move |current_state: Option<Res<State<S>>>| match current_state {
+            Some(current_state) => !(*current_state == state),
+            None => false,
+        }
+    }
+}
